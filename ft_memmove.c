88927256af8c84023eab243ed54731bb517d3160
Chukyy <51385693+Chukyy712@users.noreply.github.com>
2022-10-27 10:50:39 +0100

    bugfixes

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,26 +12,13 @@
 
 #include "libft.h"
 
-<<<<<<< HEAD
-void	*ft_memmove(void *dst, const void *src, size_t n)
-=======
 void	*ft_memmove(void *dest, const void *src, size_t n)
->>>>>>> 61dff75 (bugfixes)
 {
 	size_t			i;
 	unsigned char	*strs;
 	unsigned char	*strd;
 
 	i = -1;
-<<<<<<< HEAD
-	if (!src || !dest)
-		return (NULL);
-	strs = (unsigned char *)src;
-	strd = (unsigned char *)dst;
-	if (strd > strs)
-	{
-		while(str[q])
-=======
 	strd = (unsigned char *)dest;
 	strs = (unsigned char *)src;
 	if (!strd || !strs)
@@ -40,17 +27,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	{
 		while (0 < n--)
 			strd[n] = strs[n];
->>>>>>> 61dff75 (bugfixes)
 	}
 	else
 	{
 		while (++i < n)
-<<<<<<< HEAD
-			strd[i] = strs[i];
-	}
-=======
 			strd[i] = strd[i];
 	}
 	return (dest);
->>>>>>> 61dff75 (bugfixes)
 }
