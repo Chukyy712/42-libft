4642723b4ec26d592d26043840ea90d3ab76a051
Chukyy <51385693+Chukyy712@users.noreply.github.com>
2022-10-27 16:55:09 +0100

    UpdateV04

--- ft_memchr.c
+++ ft_memchr.c
@@ -9,3 +9,24 @@
 /*   Updated: 2022/10/16 17:57:21 by brcaetan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *src, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	chr;
+
+	str = (unsigned char *) src;
+	chr = (unsigned char) c;
+	i = -1;
+	if (!str)
+		return (0);
+	while (str[++i] || i < n)
+	{
+		if (str[i] == chr)
+			return ((char *)&str[i]);
+	}
+	return (NULL);
+}
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -9,3 +9,24 @@
 /*   Updated: 2022/10/16 17:57:21 by brcaetan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	if (str1 == str2 || (!str1 && !str2))
+		return (0);
+	if (ft_strlen((const char *)str1) < n
+		|| ft_strlen((const char *)str2) < n)
+		return (0);
+	while (i < n && str1[i] == str2[i])
+		i++;
+	return (&str1[i] - &str2[i]);
+}
