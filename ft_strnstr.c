1ac7743e2193d16a4aec5f8785242c00cde9ff46
Chukyy <51385693+Chukyy712@users.noreply.github.com>
2022-10-30 01:50:42 +0100

    UpdateV05

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,16 +6,30 @@
 /*   By: brcaetan <brcaetan@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 17:57:16 by brcaetan          #+#    #+#             */
-/*   Updated: 2022/10/16 17:57:21 by brcaetan         ###   ########.fr       */
+/*   Updated: 2022/10/30 01:37:06 by brcaetan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// char *ft_strnstr(const char *big, const char *little, size_t len)
-// {
-// 	size_t	i;
-	
+char	*ft_strnstr(const char *str, const char *find, size_t len)
+{
+	size_t	i;
+	size_t	j;
 
-
-// }
+    i = 0;
+	if (!find)
+		return ((char *)str);
+	while (str[i] && i < len)
+	{
+        j = 0;
+		while (str[i + j] == find[j])
+		{
+			if (find[j + 1] == '\0')
+				return ((char *)&(str[i]));
+            j++;
+		}
+        i++;
+	}
+	return (NULL);
+}
