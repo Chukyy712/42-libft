a5b5fe3b9b83d22f4211ae36d31344115b70a1bc
Bruno Varandas Caetano <brcaetan@c1r13s5.42lisboa.com>
2022-11-02 18:55:38 +0000

    part2wp

--- ft_substr.c
+++ ft_substr.c
@@ -12,4 +12,15 @@
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+//The string from which to create the substring.
+//start: The start index of the substring in the string ’s’.
+//len: The maximum length of the substring.
+
+	size_t	i;
+
+	while ()
+
+// Return (the substring ||NULL if the allocation fails)
+}
