328175a22898580cbd322a6363195c99adbcfc92
Bruno Varandas Caetano <brcaetan@c1r13s5.42lisboa.com>
2022-11-02 17:58:10 +0000

    Update V07

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,7 +5,7 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: brcaetan <brcaetan@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/16 17:57:16 by brcaetan          #+#    #+#             */
+/*   Created: 2022/11/02 17:57:16 by brcaetan          #+#    #+#             */
 /*   Updated: 2022/10/16 17:57:21 by brcaetan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -16,16 +16,19 @@ size_t	ft_strlcat(char *d, const char *s, size_t size)
 {
 	size_t	i;
 	size_t	lend;
+	size_t	temp;
 	size_t	lens;
 
-	lend = ft_strlen(d);
-	i = 0;
+	lens = ft_strlen(s);
 	if (!size)
-		return (ft_strlen(s));
-	if (ft_strlen(d) > size)
-		return (ft_strlen(s) + size);
+		return (lens);
+	i = 0;
+	lend = ft_strlen(d);
+	temp = lend;
+	if (lend > size)
+		return (lens + size);
 	while (s[i] && (lend < size - 1))
 		d[lend++] = s[i++];
 	d[lend] = '\0';
-	return (lend + ft_strlen(s));
+	return (temp + lens);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -1,37 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brcaetan <brcaetan@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/02 17:57:16 by brcaetan          #+#    #+#             */
-/*   Updated: 2022/10/30 01:37:06 by brcaetan         ###   ########.fr       */
+/*   Created: 2022/10/16 17:57:16 by brcaetan          #+#    #+#             */
+/*   Updated: 2022/10/16 17:57:18 by brcaetan          #+#    #+#             */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *find, size_t len)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	if (find[0] == '\0')
-		return ((char *)str);
-	if (!len)
-		return (NULL);
-	while (str[i] && i < len)
-	{
-		j = 0;
-		while (str[i + j] == find[j] && (i + j) < len)
-		{
-			if (find[j + 1] == '\0')
-				return ((char *)&(str[i]));
-			j++;
-		}
-		i++;
-	}
-	return (NULL);
-}
+char	*ft_substr(char const *s, unsigned int start, size_t len);
